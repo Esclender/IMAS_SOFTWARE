eaa862149c671dedfd67830017c43a2b4aaa61db
Esclender-Lugo <e00181703@certus.edu.pe>
2023-04-29 06:30:44 -0500

    INTERFAZ PRINCIPAL && EXCEL READER:: SHOW TABLES

--- src/imas_software/IMAS_SOFTWARE.java
+++ src/imas_software/IMAS_SOFTWARE.java
@@ -4,6 +4,13 @@
  */
 package imas_software;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  *
  * @author Esclender
@@ -15,6 +22,31 @@ public class IMAS_SOFTWARE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public String LeerExcel() throws FileNotFoundException, IOException{
+           FileInputStream fileIn = new FileInputStream("D:\\\\PRUEBAXLS.xlsx");
+           XSSFWorkbook wb = new XSSFWorkbook(fileIn);
+           XSSFSheet sheet = wb.getSheetAt(0);
+    
+           int numero_filas = sheet.getLastRowNum();
+           String tabla = "";
+            
+           for(int i = 0; i < numero_filas; i++){
+                int columnas = sheet.getRow(i).getLastCellNum();
+                
+                for(int j = 0; j < columnas; j++){
+                    XSSFCell celda = sheet.getRow(i).getCell(j);
+                    if((j + 1) == columnas){
+                        tabla += celda + "\n";
+                    }else{
+                        tabla += celda + "    ";
+                    }
+                }
+           }
+           
+           return tabla;
+           
     }
     
 }
